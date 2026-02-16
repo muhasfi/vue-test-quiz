16ddb648a593963215c23824307eb5c9842fdf99
muhharis99 <muhammadharischaidir90@gmail.com>
2026-02-16 19:47:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from "vue";
+import srcQuiz from "./data/quiz.json";
+import QuizCard from "./components/QuizCard.vue";
+
+const quizes = ref(srcQuiz);
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1 id="title">Quiz Vue</h1>
+      <input type="text" id="search-input" />
+    </header>
+    <section id="quiz-container">
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img" :alt="quiz.title" />
+        <div class="card-body">
+          <h2>{{ quiz.title }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div>
+      </div> -->
+      <QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+header {
+  margin-top: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#title {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+#search-input {
+  border: none;
+  background-color: rgb(184, 184, 184);
+  padding: 10px;
+  border-radius: 10px;
+}
+
+#quiz-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+}
+
+.card {
+  width: 270px;
+  margin-right: 30px;
+  margin-bottom: 30px;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ src/components/QuizCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+const { quiz } = defineProps(["quiz"]);
+// console.info(quiz);
+</script>
+
+<template>
+  <div class="card">
+    <img :src="quiz.img" :alt="quiz.title" />
+    <div class="card-body">
+      <h2>{{ quiz.title }}</h2>
+      <p>{{ quiz.questions.length }} Questions</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  margin: 0;
+}
+.card-body {
+  padding: 0 15px;
+}
+
+.card-body h2 {
+  font-weight: bold;
+}
+</style>
