4036a6ec9b58d12f1a47e3b6f02c1517082bcef9
muhasfi <muhammadhasfi15@gmail.com>
2026-03-17 15:13:23 +0700

    fix: add build configuration to resolve CSS preload error

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import vueDevTools from "vite-plugin-vue-devtools";
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: "/",
+  build: {
+    cssCodeSplit: false, // ← ini yang fix error preload CSS
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
