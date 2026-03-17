54de4a4221d082c88da271d6318ffa081a206112
muhasfi <muhammadhasfi15@gmail.com>
2026-03-17 15:06:24 +0700

    feat: add vercel configuration and update vite config for base path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-  base: "./", // 🔥 WAJIB TAMBAHKAN INI
-  plugins: [vue(), vueDevTools()],
+  base: "/", // 🔥 ubah dari './' jadi '/'
+  plugins: [
+    vue(),
+    // ❌ matikan dulu ini
+    // vueDevTools(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
